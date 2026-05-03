6597c3f3eb3b2f1192b2a8357596b5c2c3c65e3f
Carolina Moura <carolinamouravalle@yahoo.com>
2026-05-03 19:18:07 -0400

    Write server tests

--- server_test.go
+++ server_test.go
@@ -21,3 +21,34 @@
 //   Expected: `{"id":"<uuid2>","type":"peer-joined"}`
 
 package main
+
+import (
+	"net/http/httptest"
+	"testing"
+
+	"6.5840-final-project/clerk"
+	"github.com/gofiber/fiber/v3"
+	"github.com/stretchr/testify/assert"
+	"go.etcd.io/etcd/tests/v3/framework/integration"
+)
+
+func makeTestServer(t *testing.T, rsmClusterSize int) (*Server, func()) {
+	t.Helper()
+	ck, cleanup := clerk.MakeTestClerk(t, rsmClusterSize, integration.WithoutGoLeakDetection())
+	s := &Server{
+		rooms: make(map[string]map[string]*peer),
+		app:   fiber.New(),
+		ck:    ck,
+	}
+	s.registerRoutes()
+	return s, cleanup
+}
+
+func TestCreateRoom(t *testing.T) {
+	s, cleanup := makeTestServer(t, 1)
+	defer cleanup()
+
+	req := httptest.NewRequest("POST", "/api/rooms", nil)
+	resp, _ := s.app.Test(req)
+	assert.Equal(t, fiber.StatusCreated, resp.StatusCode)
+}

236bc08459c8b488d935486ffe814b5d4508073c
rekomodo <jairrs@mit.edu>
2026-05-07 21:28:01 -0400

    fixed bug where receiving a broadcast would mark local unbroadcasted commits as broadcasted

--- web/src/lib/textPeer.ts
+++ web/src/lib/textPeer.ts
@@ -125,8 +125,13 @@ export class TextPeer {
       this.text.applyUpdate(msg.items);
       this.text.applyTombstones(msg.deletes);
       this.refreshTombstoneOrder();
-      this.lastBroadcastSV = this.text.getStateVector();
-      this.lastBroadcastTombstoneCount = this.tombstoneOrder.length;
+      // merge the received items' implicit SV into lastBroadcastSV
+      // we do NOT use the new state vector because it would mark the local edits as broadcasted
+      for (const item of msg.items) {
+        const cur = this.lastBroadcastSV.get(item.id.userID) ?? 0;
+        const next = item.id.opCounter + 1;
+        if (next > cur) this.lastBroadcastSV.set(item.id.userID, next);
+      }
       console.log("applyUpdate", peerId, JSON.stringify(msg));
       return;
     }
